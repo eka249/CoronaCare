4ebf6368e9d77ca19235e675d2301049d377029c
Ellyn <eka249@nyu.edu>
2020-03-24 14:43:30 -0400

    added db v1

---
+++ corona-care/app/controllers/convos_controller.rb
@@ -0,0 +1,2 @@
+class ConvosController < ApplicationController
+end
--- corona-care/config/routes.rb
+++ corona-care/config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  resources :responses
+  resources :requests
+  resources :messages
+  resources :convos
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

dca23b5f35fe232bb46c5b2d501d5f6de28ae32d
Ellyn <eka249@nyu.edu>
2020-04-07 00:51:59 -0400

    fixed create userrrrrr

---
+++ corona-care/.history/src/NavBar_20200406225957.js
@@ -0,0 +1,142 @@
+import React, { Component } from "react";
+import SignInModal from "./SignInModal";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+// import NewModal from "./newModal";
+import { Input, Menu } from "semantic-ui-react";
+
+class NavBar extends Component {
+  state = {
+    activeItem: "home",
+    showNewModal: false,
+    showSignInModal: false,
+    logged_in: this.props.logged_in,
+    user: [],
+    username: "",
+    password: ""
+  };
+
+  handleItemClick = (e, { name }) => {
+    this.setState({ activeItem: name });
+  };
+
+  handleNewRequest = (e, { name }) => {
+    this.setState({ activeItem: name, showNewModal: true });
+  };
+
+  handleClickSignIn = () => {
+    console.log("hit handleClickSignIn");
+    {
+      this.setState({
+        showSignInModal: !!this.state.showSignInModal
+      });
+    }
+  };
+  // handleSignIn = (e, { name }) => {
+  //   // user will sign in and recieve token if user is valid
+  //   e.preventDefault();
+
+  //   fetch("http://localhost:3000/auth", {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //       Accepts: "application/json",
+  //       Authorization: `Bearer ${localStorage.token}`
+  //     },
+  //     body: JSON.stringify({
+  //       username: this.state.username,
+  //       password: this.state.password
+  //     })
+  //   })
+  //     .then(response => response.json())
+  //     .then(response => console.log("this is the response", response))
+  //     .then(json => {
+  //       if (json.jwt) {
+  //         localStorage.setItem("token", json.jwt);
+  //         //user then will send the token json to sign in function to recieve user data
+  //         this.getLoggedIn(json);
+  //       }
+  //     })
+  //     .then(this.setState({ activeItem: name }));
+  // };
+
+  // getLoggedIn = data => {
+  //   fetch(
+  //     "http://localhost:3000/user/profile",
+  //     {
+  //       method: "GET",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //         Accepts: "application/json",
+  //         Authorization: `Bearer ${localStorage.token}`
+  //       }
+  //     }.then(response => response.json())
+  //   ).then(this.setState({ user: data.user, logged_in: true }));
+  // };
+
+  render() {
+    const { activeItem } = this.state;
+
+    if (this.state.logged_in) {
+      return (
+        <div>
+          <Router>
+            <nav>
+              <Menu>
+                <Menu.Item>CoronaCare</Menu.Item>
+                <Menu.Item
+                  name="home"
+                  active={activeItem === "home"}
+                  onClick={this.handleItemClick}
+                >
+                  <Link to="/"> Home </Link>
+                </Menu.Item>
+                <Menu.Item
+                  name="Messages"
+                  active={activeItem === "Create a Request"}
+                  onClick={this.handleItemClick}
+                >
+                  <Link to="/messages">Messages</Link>
+                </Menu.Item>
+                <Menu.Menu position="right">
+                  <Menu.Item>
+                    <Input icon="search" placeholder="Search..." />
+                  </Menu.Item>
+                  <Menu.Item
+                    name="Logout"
+                    active={activeItem === "Logout"}
+                    onClick={this.handleItemClick}
+                    //finish log out onClick when user is set up
+                  ></Menu.Item>
+                </Menu.Menu>
+              </Menu>
+            </nav>
+          </Router>
+
+          {this.state.showSignInModal ? (
+            <SignInModal handleClickSignIn={this.handleClickSignIn()} />
+          ) : null}
+        </div>
+      );
+    }
+
+    return (
+      <div>
+        <nav>
+          <Menu>
+            <Menu.Item>CoronaCare</Menu.Item>
+            <Menu.Menu position="right">
+              <Menu.Item
+                name="Log In"
+                active={activeItem === "Log In"}
+                onClick={this.handleClickSignIn}
+              ></Menu.Item>
+            </Menu.Menu>
+          </Menu>
+        </nav>
+        {this.state.showSignInModal ? <SignInModal /> : null}
+      </div>
+    );
+  }
+}
+
+export default NavBar;
---
+++ corona-care/.history/src/SignInModal_20200406221507.js
@@ -0,0 +1,162 @@
+import React, { Component } from "react";
+import { Modal, Form, Header, Button } from "semantic-ui-react";
+
+class SignInModal extends Component {
+  state = {
+    username: "",
+    password: "",
+    newFirstName: "",
+    newLastName: "",
+    newUsername: "",
+    newPhone: "",
+    open: true
+  };
+  handleChange = e => {
+    let inputVal = e.target.name;
+    this.setState({ ...this.state, inputVal: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    //routes to auth#create on backend to recieve token
+    fetch("http://localhost:3000/auth", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accepts: "application/json",
+        Authorization: `Bearer ${localStorage.token}`
+      },
+      body: JSON.stringify({
+        username: this.state.username,
+        password: this.state.password
+      })
+    })
+      .then(response => response.json())
+      .then(json => {
+        if (json.jwt) {
+          localStorage.setItem("token", json.jwt);
+          this.props.getLoggedIn(json);
+        }
+      })
+      .then();
+  };
+
+  handleSignUp = () => {
+    console.log("started making new user");
+    fetch("http://localhost:3000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        Authorization: `Bearer ${localStorage.token}`
+      },
+      body: JSON.stringify({
+        newFirstName: this.state.newFirstName,
+        password_digest: this.state.password,
+        newLastName: this.state.newLastName,
+        newPhone: this.state.newPhone
+      })
+    }).then(response => response.json());
+  };
+
+  render() {
+    return (
+      <Modal
+        // as={Form}
+        // onSubmit={e => this.handleSignUp(e)}
+        open={this.state.open}
+        // size="tiny"
+        // closeIcon={this.props.showModal}
+        // className="c-modal"
+        // trigger={this.state.open}
+      >
+        <Header content="Sign In" as="h2"></Header>
+        <Modal.Content>
+          <Form.Input
+            label="Username "
+            name="username"
+            // required
+            type="text"
+            placeholder="Username"
+            id="username"
+            onChange={this.handleChange}
+          />
+          <Form.Input
+            label="Password "
+            // required
+            name="password"
+            type="password"
+            placeholder="Password"
+            id="password"
+            onChange={this.handleChange}
+          />
+        </Modal.Content>
+        <Modal.Actions>
+          <Button
+            content="Sign In"
+            // onClick={console.log("sign in hit")}
+            // onClick={
+            //   () => this.onSignIn()
+
+            // this.props.showModal
+            // }
+          />
+        </Modal.Actions>
+        <Modal.Content>
+          <Header content="Or Sign Up!" as="h3"></Header>
+
+          <Form.Input
+            label="Your First Name"
+            // required
+            type="text"
+            placeholder="User"
+            name="newFirstName"
+            id="newFirstName"
+            onChange={this.handleChange}
+          />
+          <Form.Input
+            label="Your Last Initial"
+            // required
+            type="text"
+            placeholder="User"
+            name="newLastName"
+            id="newLastName"
+            onChange={this.handleChange}
+          />
+          <Form.Input
+            label="Your Phone Number"
+            // required
+            type="text"
+            placeholder="User"
+            name="newPhone"
+            id="newPhone"
+            onChange={this.handleChange}
+          />
+          <Form.Input
+            label=" New username "
+            // required
+            type="text"
+            placeholder="Username"
+            name="newUsername"
+            id="newUsername"
+            onChange={this.handleChange}
+          />
+          <Form.Input
+            label=" New password "
+            // required
+            type="password"
+            placeholder="Password"
+            name="newPassword"
+            id="newPassword"
+            onChange={this.handleChange}
+          />
+        </Modal.Content>
+        <Modal.Actions>
+          <Button type="submit" onClick={this.handleSignUp} content="Sign Up" />
+        </Modal.Actions>
+      </Modal>
+    );
+  }
+}
+
+export default SignInModal;
